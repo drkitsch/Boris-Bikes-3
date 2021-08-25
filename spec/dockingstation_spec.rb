ae9b23069830dffd16503901dc27996d823e8b3a
Ifrah Hussein <Ifrahhssn@outlook.com>
2021-08-25 16:33:59 +0100

    Added unit test to DockingStation

--- spec/dockingstation_spec.rb
+++ spec/dockingstation_spec.rb
@@ -1,5 +1,10 @@
-require './lib/borisbikes.rb'
+require './lib/dockingstation.rb'
 
 describe DockingStation do
     it { is_expected.to respond_to :release_bike }
+    
+    it 'releases bikes that work' do
+        bike = subject.release_bike 
+        expect(bike).to be_working
+    end    
 end 
